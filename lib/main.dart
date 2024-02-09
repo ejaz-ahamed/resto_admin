b684beb28d0262374d5ef56cbf546018d3667f82
rashi8643 <rashimuhammed86@gmail.com>
2024-02-09 15:56:12 +0530

    Constants added

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:resto_admin/core/themes/light_theme.dart';
 import 'package:resto_admin/feature_orders/presentation/pages/order_view_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
+      theme: ref.watch(lightThemeProvider),
       home: const OrderDetalisPage(),
     );
   }
