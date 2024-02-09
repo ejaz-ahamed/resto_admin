724fea545405858bd3d0a3633eee42ec879e5aba
rashi8643 <rashimuhammed86@gmail.com>
2024-02-09 14:18:13 +0530

    added core ande app themes

---
+++ lib/core/themes/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:resto_admin/core/themes/extensions/color_extension.dart';
+import 'package:resto_admin/core/themes/extensions/space_extension.dart';
+import 'package:resto_admin/core/themes/extensions/typography_extension.dart';
+
+final class AppTheme {
+  final BuildContext context;
+
+  AppTheme.of(this.context);
+
+  AppColorExtension get colors {
+    return Theme.of(context).extension<AppColorExtension>()!;
+  }
+
+  AppSpaceExtension get spaces {
+    return Theme.of(context).extension<AppSpaceExtension>()!;
+  }
+
+  AppTypographyExtension get typography {
+    return Theme.of(context).extension<AppTypographyExtension>()!;
+  }
+}
---
+++ lib/core/themes/color_palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColorPalettes {
+  static const red400 = Color(0xFFFF4545);
+  static const blue900 = Color(0xFF172B4D);
+  static const grey400 = Color(0xFF626F86);
+  static const white500 = Colors.white;
+  static const black500 = Colors.black;
+  static const gray300 = Color(0xFFd9d9d9);
+  static const gray200 = Color(0xFFd9dce1);
+}
