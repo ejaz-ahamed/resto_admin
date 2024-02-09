ac0b6b953dfb670e00eac67da03b3b9323d42a63
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-02-09 16:09:55 +0530

    ghvn

--- lib/core/constants/products_constants/product_constants.dart
+++ lib/core/constants/products_constants/product_constants.dart
@@ -3,8 +3,8 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'product_constants.g.dart';
 
 class ProductConstants {
-  final txtaddPdtTitle = 'Add New Products';
-  final txtproductTitle = 'Products';
+  final txtAddPrdtsTitle = 'Add New Products';
+  final txtProductTitle = 'Products';
 }
 
 @riverpod
