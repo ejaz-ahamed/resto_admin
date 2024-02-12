3a6e243572f38a69ec280f4fa3ba25c999abdd87
MuhammedFaasil <muhammedfaasilpty@gmail.com>
2024-02-12 15:02:15 +0530

    bottonavigation bar added

--- lib/features/authentication/presentation/widgets/bottom_nav_widget.dart
+++ lib/features/authentication/presentation/widgets/bottom_nav_widget.dart
@@ -1,58 +1,129 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:resto_admin/core/themes/app_theme.dart';
 
-class BottomNaviWidget extends StatelessWidget {
+class BottomNaviWidget extends StatefulWidget {
   const BottomNaviWidget({super.key});
 
   @override
+  State<BottomNaviWidget> createState() => _BottomNaviWidgetState();
+}
+
+class _BottomNaviWidgetState extends State<BottomNaviWidget> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
+    final theme = AppTheme.of(context);
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 32),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(60),
-        child: Container(
+      child: Container(
           width: 382,
-          height: 72,
+          height: 75,
           decoration: BoxDecoration(
-              color: Colors.white,
+              // color: Colors.white,
               borderRadius: BorderRadius.circular(60),
-              boxShadow: [BoxShadow(color: Color(000000), blurRadius: 0.25)]),
-          child: BottomNavigationBar(
-              backgroundColor: Colors.white,
-              selectedItemColor: Colors.red,
+              boxShadow: [
+                BoxShadow(
+                    color: AppTheme.of(context).colors.textSubtle,
+                    blurRadius: 0.25,
+                    spreadRadius: 0,
+                    offset: Offset(0, 4))
+              ]),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(60),
+            child: BottomNavigationBar(
+              // selectedItemColor: Colors.white,
+              // unselectedItemColor: Colors.red,
+              currentIndex: selectedIndex,
+              // backgroundColor: Colors.white,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              onTap: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
               items: [
                 BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      'asset/icons/ic_home_icon.svg',
-                      height: 24,
-                      width: 24,
-                      color: Colors.red,
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(96),
+                          color: selectedIndex == 0
+                              ? theme.colors.primary
+                              : Colors.white),
+                      child: Center(
+                        child: SvgPicture.asset(
+                          'asset/icons/ic_home_icon.svg',
+                          height: 20,
+                          width: 20,
+                          color: selectedIndex == 0 ? Colors.white : Colors.red,
+                        ),
+                      ),
                     ),
                     label: ''),
                 BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      'asset/icons/ic_bowl_icon.svg',
-                      height: 24,
-                      width: 24,
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(96),
+                          color: selectedIndex == 1
+                              ? theme.colors.primary
+                              : Colors.white),
+                      child: Center(
+                        child: SvgPicture.asset(
+                          'asset/icons/ic_bowl_icon.svg',
+                          height: 24,
+                          width: 20,
+                          color: selectedIndex == 1 ? Colors.white : Colors.red,
+                        ),
+                      ),
                     ),
                     label: ''),
                 BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      'asset/icons/ic_bag_icon.svg',
-                      height: 24,
-                      width: 24,
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(96),
+                          color: selectedIndex == 2
+                              ? theme.colors.primary
+                              : Colors.white),
+                      child: Center(
+                        child: SvgPicture.asset(
+                          'asset/icons/ic_bag_icon.svg',
+                          height: 24,
+                          width: 20,
+                          color: selectedIndex == 2 ? Colors.white : Colors.red,
+                        ),
+                      ),
                     ),
                     label: ''),
                 BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      'asset/icons/ic_offer_icon.svg',
-                      height: 24,
-                      width: 24,
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(96),
+                          color: selectedIndex == 3
+                              ? theme.colors.primary
+                              : Colors.white),
+                      child: Center(
+                        child: SvgPicture.asset(
+                          'asset/icons/ic_offer_icon.svg',
+                          height: 24,
+                          width: 20,
+                          color: selectedIndex == 3 ? Colors.white : Colors.red,
+                        ),
+                      ),
                     ),
                     label: ''),
-              ]),
-        ),
-      ),
+              ],
+            ),
+          )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:resto_admin/core/themes/light_theme.dart';
 import 'package:resto_admin/features/authentication/presentation/pages/homepage.dart';
-import 'package:resto_admin/features/authentication/presentation/widgets/bottom_nav_widget.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
     return MaterialApp(
+      theme: ref.watch(lightThemeProvider),
       title: 'Flutter SAMPLEEEE',
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
