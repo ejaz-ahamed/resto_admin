737cf140736a0e2292fd6ccd3dcdafc1bf5e6ff5
epashiq <ashiqashi4715@gmail.com>
2024-02-12 12:31:42 +0530

    resolving conflicts'

--- lib/features/products/presentation/pages/product_page.dart
+++ lib/features/products/presentation/pages/product_page.dart
@@ -27,6 +27,34 @@ class ProductPage extends ConsumerWidget {
           ],
         ),
       ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: AppTheme.of(context).spaces.space_400,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: AppTheme.of(context).spaces.space_300),
+            child: InkWell(
+              child: Container(
+                  height: MediaQuery.sizeOf(context).height / 3.8,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: AppTheme.of(context).colors.textSubtle,
+                          width: AppTheme.of(context).spaces.space_25),
+                      borderRadius: BorderRadius.circular(
+                          AppTheme.of(context).spaces.space_100)),
+                  child: Column(children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon:
+                            SvgPicture.asset('assets/icons/ic_add_image.svg')),
+                  ])),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
