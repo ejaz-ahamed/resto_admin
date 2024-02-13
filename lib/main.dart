81d1d1a638298f31afc732b39c65fd6ecc99b44f
epashiq <ashiqashi4715@gmail.com>
2024-02-13 18:25:05 +0530

    update product page

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:resto_admin/core/router/router.dart';
 import 'package:resto_admin/core/themes/light_theme.dart';
-import 'package:resto_admin/features/products/presentation/pages/product_page.dart';
-
-// void main() {
-//   runApp(const ProviderScope(child: MyApp()));
-// }
-
-// class MyApp extends ConsumerWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     return MaterialApp.router(
-//       title: 'Flutter SAMPLEEEE',
-//       debugShowCheckedModeBanner: false,
-//       theme: ref.watch(lightThemeProvider),
-//       routerConfig: ref.watch(goRouterProvider),
-//     );
-//   }
-// }
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -31,11 +12,13 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter SAMPLEEEE',
       debugShowCheckedModeBanner: false,
       theme: ref.watch(lightThemeProvider),
-      home: ProductPage(),
+      routerConfig: ref.watch(goRouterProvider),
     );
   }
 }
+
+
