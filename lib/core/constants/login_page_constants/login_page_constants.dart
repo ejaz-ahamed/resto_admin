9f255b7ddc3a7e9a3d94036c48f20be66b43267f
MUHAMMED DILSHAD KT <dilshadktkunnathodi@gmail.com>
2024-02-14 10:13:25 +0530

    login page constants modified

--- lib/core/constants/login_page_constants/login_page_constants.dart
+++ lib/core/constants/login_page_constants/login_page_constants.dart
@@ -2,8 +2,8 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'login_page_constants.g.dart';
 
 class LoginPageConstants {
-  final String heading = "WELCOME BACK!";
-  final String buttonText = "LOGIN";
+  final String txtHeading = "WELCOME BACK!";
+  final String txtLoginBtn = "LOGIN";
   final String userName = "User Name";
   final String password = "Password";
 }
