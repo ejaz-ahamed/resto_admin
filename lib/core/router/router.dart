dff1f39144ee67fcc89520a61c37c611a2220ffa
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-02-14 09:57:34 +0530

    merge conflicts resolved

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -4,7 +4,6 @@ import 'package:resto_admin/features/edit_profile_page/presentation/pages/edit_p
 import 'package:resto_admin/features/edit_profile_page/presentation/pages/edit_profile_page.dart';
 import 'package:resto_admin/features/offer/presentation/pages/add_offer_page.dart';
 import 'package:resto_admin/features/products/presentation/pages/home_page.dart';
-import 'package:resto_admin/features/profile_page/presentation/pages/profile_page.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'router.g.dart';
