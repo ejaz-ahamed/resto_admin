b28d9b3d4238f2b7cb58b7995e325914accca3e7
muhammed-sinan-vadakara <muhammedsinan5034@gmail.com>
2024-02-16 18:01:42 +0530

    offerpage model class and datasouce adding

---
+++ lib/features/offer/domain/usecase/add_offer_usecase.dart
@@ -0,0 +1,29 @@
+import 'package:resto_admin/core/enums/offer_type.dart';
+import 'package:resto_admin/core/exceptions/base_exception/base_exception.dart';
+import 'package:resto_admin/features/offer/domain/entity/offer_entity.dart';
+import 'package:resto_admin/features/offer/domain/repository/offer_repository.dart';
+
+final class AddOfferUseCase {
+  final OfferRepository repository;
+  AddOfferUseCase({required this.repository});
+  Future<void> call({
+    required String id,
+    required String imagePath,
+    required String name,
+    required String description,
+    required OfferType offerType,
+    required List<String> product,
+  }) async {
+    try {
+      return await repository.addOffer(OfferEntity(
+          id: id,
+          imagePath: imagePath,
+          name: name,
+          description: description,
+          offerType: offerType,
+          product: product));
+    } catch (e) {
+      throw BaseException(e.toString());
+    }
+  }
+}
