5232e8530cefbd456ee1e6b6be3706b481567086
muhammed-sinan-vadakara <muhammedsinan5034@gmail.com>
2024-02-16 18:03:55 +0530

    offerpage model class and datasouce adding

--- lib/features/offer/presentation/provider/offer_provider.dart
+++ lib/features/offer/presentation/provider/offer_provider.dart
@@ -1,1 +1,31 @@
+import 'package:resto_admin/core/enums/offer_type.dart';
+import 'package:resto_admin/features/offer/data/repository/offer_repository_impl.dart';
+import 'package:resto_admin/features/offer/domain/repository/offer_repository.dart';
+import 'package:resto_admin/features/offer/domain/usecase/add_offer_usecase.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+part 'offer_provider.g.dart';
+
+@riverpod
+class Offer extends _$Offer {
+  late OfferRepository repository;
+  @override
+  void build() {}
+  Future<void> addOffer({
+    required String id,
+    required String imagePath,
+    required String name,
+    required String description,
+    required OfferType offerType,
+    required List<String> product,
+  }) {
+    final repository = ref.watch(offerRepositoryProvider);
+    return AddOfferUseCase(repository: repository)(
+        id: id,
+        imagePath: imagePath,
+        name: name,
+        description: description,
+        offerType: offerType,
+        product: product);
+  }
+}
