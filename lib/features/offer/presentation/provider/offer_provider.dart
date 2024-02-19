5b757a69e0e5e9196924f8faae3cb408ad513bfd
muhammed-sinan-vadakara <muhammedsinan5034@gmail.com>
2024-02-19 17:20:10 +0530

    offer adding added

--- lib/features/offer/presentation/provider/offer_provider.dart
+++ lib/features/offer/presentation/provider/offer_provider.dart
@@ -1,14 +1,17 @@
 import 'package:resto_admin/core/enums/offer_type.dart';
 import 'package:resto_admin/features/offer/data/repository/offer_repository_impl.dart';
 import 'package:resto_admin/features/offer/domain/entity/offer_entity.dart';
+import 'package:resto_admin/features/offer/domain/repository/offer_repository.dart';
 import 'package:resto_admin/features/offer/domain/usecase/add_offer_usecase.dart';
 import 'package:resto_admin/features/offer/domain/usecase/get_offer_usecase.dart';
 import 'package:resto_admin/features/offer/domain/usecase/remove_offer_usecase.dart';
+import 'package:resto_admin/features/offer/domain/usecase/update_offer_usecase.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'offer_provider.g.dart';
 
 @riverpod
 class Offer extends _$Offer {
+  late OfferRepository repository;
   @override
   List<OfferEntity>? build() {
     return null;
@@ -39,53 +42,12 @@ class Offer extends _$Offer {
     return GetOfferUseCase(repository: repository)();
   }
 
+  Future<void> update(OfferEntity offerEntity) async {
+    return UpdateOfferUseCase(repository: repository)(offerEntity);
+  }
+
   Future<void> deleteOffer(String id) async {
     final repository = ref.watch(offerRepositoryProvider);
     await RemoveOfferUsecase(repository: repository)(id);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:resto_admin/features/offer/data/repository/offer_repository_impl.dart';
-// import 'package:resto_admin/features/offer/domain/entity/offer_entity.dart';
-// import 'package:resto_admin/features/offer/domain/repository/offer_repository.dart';
-// import 'package:resto_admin/features/offer/domain/usecase/add_offer_usecase.dart';
-// import 'package:resto_admin/features/offer/domain/usecase/remove_offer_usecase.dart';
-// import 'package:resto_admin/features/offer/domain/usecase/update_offer_usecase.dart';
-// import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-// part 'offer_provider.g.dart';
-
-// @riverpod
-// class OfferProvider extends _$OfferProvider {
-//   late OfferRepository repository;
-//   @override 
-//   void build() {}
-//   Future<void> addOffer(OfferEntity offerEntity) {
-//     return AddOfferUseCase(repository: repository)(offerEntity);
-//   }
-
-//   Future<void> deleteOffer(String id) async {
-//     final repository = ref.watch(offerRepositoryProvider);
-//     await RemoveOfferUsecase(repository: repository)(id);
-//   }
-
-//   Future<void> update(OfferEntity offerEntity) async {
-//     return UpdateOfferUseCase(repository: repository)(offerEntity);
-//   }
-// }
