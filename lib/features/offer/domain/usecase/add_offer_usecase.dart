298ca3d05d05e78868bb2f46475ba7f4374a0908
muhammed-sinan-vadakara <muhammedsinan5034@gmail.com>
2024-02-19 11:17:58 +0530

    offerpage repository and usecase added

--- lib/features/offer/domain/usecase/add_offer_usecase.dart
+++ lib/features/offer/domain/usecase/add_offer_usecase.dart
@@ -1,4 +1,3 @@
-import 'package:resto_admin/core/enums/offer_type.dart';
 import 'package:resto_admin/core/exceptions/base_exception/base_exception.dart';
 import 'package:resto_admin/features/offer/domain/entity/offer_entity.dart';
 import 'package:resto_admin/features/offer/domain/repository/offer_repository.dart';
@@ -6,24 +5,9 @@ import 'package:resto_admin/features/offer/domain/repository/offer_repository.da
 final class AddOfferUseCase {
   final OfferRepository repository;
   AddOfferUseCase({required this.repository});
-  Future<void> call({
-    required String id,
-    required String imagePath,
-    required String name,
-    required String description,
-    required OfferType offerType,
-    required List<String> product,
-    required double amount,
-  }) async {
+  Future<void> call(OfferEntity offerEntity) async {
     try {
-      return await repository.addOffer(OfferEntity(
-          id: id,
-          imagePath: imagePath,
-          name: name,
-          description: description,
-          offerType: offerType,
-          amount: amount,
-          product: product));
+      repository.addOffer(offerEntity);
     } catch (e) {
       throw BaseException(e.toString());
     }
--- lib/features/offer/presentation/provider/offer_provider.dart
+++ lib/features/offer/presentation/provider/offer_provider.dart
@@ -1,8 +1,9 @@
-import 'package:resto_admin/core/enums/offer_type.dart';
 import 'package:resto_admin/features/offer/data/repository/offer_repository_impl.dart';
+import 'package:resto_admin/features/offer/domain/entity/offer_entity.dart';
 import 'package:resto_admin/features/offer/domain/repository/offer_repository.dart';
 import 'package:resto_admin/features/offer/domain/usecase/add_offer_usecase.dart';
 import 'package:resto_admin/features/offer/domain/usecase/remove_offer_usecase.dart';
+import 'package:resto_admin/features/offer/domain/usecase/update_offer_usecase.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'offer_provider.g.dart';
@@ -12,28 +13,16 @@ class OfferProvider extends _$OfferProvider {
   late OfferRepository repository;
   @override
   void build() {}
-  Future<void> addOffer({
-    required String id,
-    required String imagePath,
-    required String name,
-    required String description,
-    required OfferType offerType,
-    required List<String> product,
-    required double amount,
-  }) {
-    final repository = ref.watch(offerRepositoryProvider);
-    return AddOfferUseCase(repository: repository)(
-        id: id,
-        imagePath: imagePath,
-        name: name,
-        description: description,
-        offerType: offerType,
-        amount: amount,
-        product: product);
+  Future<void> addOffer(OfferEntity offerEntity) {
+    return AddOfferUseCase(repository: repository)(offerEntity);
   }
 
   Future<void> deleteOffer(String id) async {
     final repository = ref.watch(offerRepositoryProvider);
     await RemoveOfferUsecase(repository: repository)(id);
+  }
+
+  Future<void> update(OfferEntity offerEntity) async {
+    return UpdateOfferUseCase(repository: repository)(offerEntity);
   }
 }
